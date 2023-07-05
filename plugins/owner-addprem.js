d85525224784cfc7b3479e4ed701ec6f85b0f052
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-05 19:47:18 +0300

    зшгп

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   var mes1 = 30 * dia1 * txt // 1 mes
   var now = Date.now()
 
-  if (command == 'addprem' || command == 'userpremium') {
+  if (command == 'датьпрем' || command == 'userpremium') {
     if (now < user.premiumTime) user.premiumTime += hora1
     else user.premiumTime = now + hora1
     user.premium = true
@@ -28,7 +28,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let textprem1 = `*🎟️ 𝙽𝚄𝙴𝚅𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼!!!*\n\n*✨ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾: ${name}*\n*🕐 𝚃𝙸𝙴𝙼𝙿𝙾: ${txt} hora(s)*\n*📉 𝚁𝙴𝚂𝚃𝙰𝙽𝚃𝙴: ${timeLeft} segundos*`
     m.reply(textprem1, null, { mentions: conn.parseMention(textprem1) })}
     
-  if (command == 'addprem2' || command == 'userpremium2') {
+  if (command == 'датьпрем2' || command == 'userpremium2') {
     if (now < user.premiumTime) user.premiumTime += dia1
     else user.premiumTime = now + dia1
     user.premium = true
@@ -36,7 +36,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let textprem2 = `*🎟️ 𝙽𝚄𝙴𝚅𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼!!!*\n\n*✨ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾: ${name}*\n*🕐 𝚃𝙸𝙴𝙼𝙿𝙾: ${txt} día(s)*\n*📉 𝚁𝙴𝚂𝚃𝙰𝙽𝚃𝙴: ${timeLeft} horas*`
     m.reply(textprem2, null, { mentions: conn.parseMention(textprem2) })}
   
-  if (command == 'addprem3' || command == 'userpremium3') {
+  if (command == 'датьпрем3' || command == 'userpremium3') {
     if (now < user.premiumTime) user.premiumTime += semana1
     else user.premiumTime = now + semana1
     user.premium = true
@@ -44,7 +44,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let textprem3 = `*🎟️ 𝙽𝚄𝙴𝚅𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼!!!*\n\n*✨ 𝚄𝚂𝚄𝙰𝚁𝙸𝙾: ${name}*\n*🕐 𝚃𝙸𝙴𝙼𝙿𝙾: ${txt} semana(s)*\n*📉 𝚁𝙴𝚂𝚃𝙰𝙽𝚃𝙴: ${timeleft}*`
     m.reply(textprem3, null, { mentions: conn.parseMention(textprem3) })})}
   
-  if (command == 'addprem4' || command == 'userpremium4') {
+  if (command == 'датьпрем4' || command == 'userpremium4') {
     if (now < user.premiumTime) user.premiumTime += mes1
     else user.premiumTime = now + mes1
     user.premium = true
@@ -54,7 +54,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['addprem [@user] <days>']
 handler.tags = ['owner']
-handler.command = ['addprem', 'userpremium', 'addprem2', 'userpremium2', 'addprem3', 'userpremium3', 'addprem4', 'userpremium4'] 
+handler.command = ['датьпрем', 'userpremium', 'датьпрем2', 'userpremium2', 'датьпрем3', 'userpremium3', 'датьпрем4', 'userpremium4'] 
 handler.group = true
 handler.rowner = true
 export default handler
